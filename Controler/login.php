d57d508d2530405d3f7da1e5e1ec81c280a7a2e9
Luis Fernando Olmos Berrio <luisfernandoolmosberrio56@gmail.com>
2024-09-30 14:31:09 -0500

    init with Vuejs

--- Controler/login.php
+++
@@ -1,4 +0,0 @@
-<?php
-    function (array $data): array {
-
-    }
--- Vew/PHP/login.php
+++
@@ -1,9 +0,0 @@
-<?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-        $userName = $_POST['userName'];
-        $password = $_POST['password'];
-    
-        echo "Nombre: " . $userName . "<br>";
-        echo "password: " . $userName;
-    }
